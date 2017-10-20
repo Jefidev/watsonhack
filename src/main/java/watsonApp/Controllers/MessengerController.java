5536a32ca3ccd927e18a3a440e55fb5461cae6a5
jefidev <jerome.fink@student.unamur.be>
2017-10-20 17:14:56 +0200

    Experimenting with webhook

---
+++ src/main/java/watsonApp/Controllers/MessengerController.java
@@ -0,0 +1,105 @@
+package watsonApp.Controllers;
+
+import com.github.messenger4j.MessengerPlatform;
+import com.github.messenger4j.exceptions.MessengerApiException;
+import com.github.messenger4j.exceptions.MessengerIOException;
+import com.github.messenger4j.exceptions.MessengerVerificationException;
+import com.github.messenger4j.receive.MessengerReceiveClient;
+import com.github.messenger4j.receive.handlers.TextMessageEventHandler;
+import com.github.messenger4j.send.MessengerSendClient;
+import com.github.messenger4j.send.NotificationType;
+import com.github.messenger4j.send.Recipient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+import static com.github.messenger4j.MessengerPlatform.*;
+
+/**
+ * Created by jfink on 20/10/17.
+ */
+
+@RestController
+public class MessengerController {
+
+    private final MessengerReceiveClient receiveClient;
+    private final MessengerSendClient sendClient;
+
+    @Autowired
+    public MessengerController(@Value("${messenger4j.appSecret}") final String appSecret,
+                               @Value("${messenger4j.verifyToken}") final String verifyToken,
+                               final MessengerSendClient sendClient){
+        this.receiveClient = MessengerPlatform.newReceiveClientBuilder(appSecret, verifyToken)
+                .onTextMessageEvent(newTextMessage()).build();
+
+        this.sendClient = sendClient;
+    }
+
+    /**
+     * Callback endpoint responsible for processing the inbound messages and events.
+     */
+    @RequestMapping(value = "/webhook", method = RequestMethod.POST)
+    public ResponseEntity<Void> handleCallback(@RequestBody final String payload,
+                                               @RequestHeader(SIGNATURE_HEADER_NAME) final String signature) {
+
+        try {
+            this.receiveClient.processCallbackPayload(payload, signature);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } catch (MessengerVerificationException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+
+
+    public TextMessageEventHandler newTextMessage(){
+        return event->{
+            final String messageId = event.getMid();
+            final String messageText = event.getText();
+            final String senderId = event.getSender().getId();
+            final Date timestamp = event.getTimestamp();
+
+            sendTextMessage(senderId, messageText);
+        };
+    }
+
+    private void sendTextMessage(String recipientId, String text) {
+        try {
+            final Recipient recipient = Recipient.newBuilder().recipientId(recipientId).build();
+            final NotificationType notificationType = NotificationType.REGULAR;
+            final String metadata = "DEVELOPER_DEFINED_METADATA";
+
+            this.sendClient.sendTextMessage(recipient, notificationType, text, metadata);
+        } catch (MessengerApiException | MessengerIOException e) {
+            System.out.printf("Oups");
+        }
+    }
+
+
+    /**
+     * Webhook verification endpoint.
+     *
+     * The passed verification token (as query parameter) must match the configured verification token.
+     * In case this is true, the passed challenge string must be returned by this endpoint.
+     */
+    @RequestMapping(value = "/webhook", method = RequestMethod.GET)
+    public ResponseEntity<String> verifyWebhook(@RequestParam(MODE_REQUEST_PARAM_NAME) final String mode,
+                                                @RequestParam(VERIFY_TOKEN_REQUEST_PARAM_NAME) final String verifyToken,
+                                                @RequestParam(CHALLENGE_REQUEST_PARAM_NAME) final String challenge) {
+
+        try {
+            return ResponseEntity.ok(this.receiveClient.verifyWebhook(mode, verifyToken, challenge));
+        } catch (MessengerVerificationException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
+    }
+
+    @RequestMapping("/messenger")
+    public String test(){
+        return "test";
+    }
+
+}
--- src/main/java/watsonApp/WatsonAppMain.java
+++ src/main/java/watsonApp/WatsonAppMain.java
@@ -1,9 +1,13 @@
 package watsonApp;
 
+import com.github.messenger4j.MessengerPlatform;
+import com.github.messenger4j.send.MessengerSendClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by jfink on 20/10/17.
@@ -15,6 +19,11 @@ public class WatsonAppMain extends SpringBootServletInitializer {
     public static void main(String[] args){
         SpringApplication.run(WatsonAppMain.class, args);
 
+    }
+
+    @Bean
+    public MessengerSendClient messengerSendClient(@Value("${messenger4j.pageAccessToken}") String pageAccessToken) {
+        return MessengerPlatform.newSendClientBuilder(pageAccessToken).build();
     }
 
     @Override
