9898e64c2398584ff24e285da1a5bbad9e55a1ce
jefidev <jerome.fink@student.unamur.be>
2017-10-21 09:05:11 +0200

    MessageContainer create Attr

--- src/main/java/watsonApp/Entities/MessageContainer.java
+++ src/main/java/watsonApp/Entities/MessageContainer.java
@@ -1,8 +1,26 @@
 package watsonApp.Entities;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Created by jfink on 21/10/17.
  */
 public class MessageContainer {
-    public MessageContainer(){}
+
+    private String text;
+    private String domain;
+    private String placeHolder;
+    private String accountNumber;
+
+
+
+    public MessageContainer(JSONObject json){
+        JSONObject output = json.getJSONObject("output");
+        JSONArray array = output.getJSONArray("text");
+
+        text=array.toString();
+
+
+    }
 }
